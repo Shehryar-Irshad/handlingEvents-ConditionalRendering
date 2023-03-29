4ee7635590c20345ab3ca9da7902aae5e6e6cc4f
Shehryar-Irshad <shehryarirshad28@gmail.com>
2023-03-29 13:01:09 +0500

    load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+//   const purple = "#8e44ad";
+//   const [bg, setBg] = useState(purple);
+//   const [name, setname] = useState('Click me');
+
+//   const BgChange = ()=> {
+//        let newBg = '#34495e';
+//        setBg(newBg);
+//        setname('Ouch !!!');
+//   }
+//   const BgBack = ()=> {
+//     let newBg = '#24463e';
+//     setBg(newBg);
+//     setname('Oho !!! :)');
+//}
+   const [user, setUser] = useState('null');
+   const load = ()  =>setUser({name: 'Jane'});
+   const clear = () =>setUser(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App" style={{backgroundColor: bg}}>
+    //       <button onClick={BgChange} onDoubleClick={BgBack}>
+    //             {name}
+    //       </button>
+    // </div>
+    <div className='App'>
+        <div>
+            <button onClick={load}>Load</button>
+            <button onClick={clear}>Clear</button>
+        </div>
+        {user && <p>{user.name}</p>}
     </div>
   );
 }
